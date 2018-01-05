6da7cd492c98bec373789072c62c37adb53f8fff
Noah Lemen <noah.lemen@gmail.com>
2018-01-05 15:48:43 -0500

    switch from preset-es2015 to basic preset-env config

--- babel-preset-r29/index.js
+++ babel-preset-r29/index.js
@@ -1,16 +1,16 @@
 'use strict';
 
 module.exports = function (context, opts = {}) {
-  var es2015 = ['es2015', {}];
+  var env = ['env', {}];
   if (opts.modules !== undefined) {
-    es2015[1].modules = opts.modules;
+    env[1].modules = opts.modules;
   }
   if (opts.loose !== undefined) {
-    es2015[1].loose = opts.loose;
+    env[1].loose = opts.loose;
   }
 
   return {
-    presets: [es2015, "react", "stage-3"],
+    presets: [env, "react", "stage-3"],
     plugins: ["transform-class-properties"]
   };
 };
